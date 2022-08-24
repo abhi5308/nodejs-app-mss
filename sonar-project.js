1d357e41c326a278261dcc084c2dabbbb9987103
abhi5308 <99579914+abhi5308@users.noreply.github.com>
2022-08-24 22:26:02 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,15 +1,15 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.127.85.206:9980/',
+  serverUrl: 'http://8.224.179.138//',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.projectKey':'NodeJsAbhi',
 	    'sonar.login': '9d988128860aaa457ff7729bdfdf996f55cce543',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
-            'sonar.projectVersion':'1.0',
+            'sonar.projectVersion':'8.3',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
             'sonar.sources': '.',
